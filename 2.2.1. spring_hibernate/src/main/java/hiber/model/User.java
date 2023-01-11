9c979c8040bf546971b5010e4bcfdfa74a95e307
XIIIDark <alexartemyev13@gmail.com>
2023-01-12 01:44:31 +0600

    da blya

--- 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/model/User.java
@@ -8,6 +8,7 @@ public class User {
 
    @Id
    @GeneratedValue(strategy = GenerationType.IDENTITY)
+   @Column(name = "id")
    private Long id;
 
    @Column(name = "name")
@@ -19,14 +20,34 @@ public class User {
    @Column(name = "email")
    private String email;
 
+
+   @OneToOne//(fetch = FetchType.LAZY)
+   //@JoinColumn(name = "car_id")
+   @MapsId
+   private Car car;
+
    public User() {}
    
    public User(String firstName, String lastName, String email) {
       this.firstName = firstName;
       this.lastName = lastName;
       this.email = email;
+
+   }
+   public User(String firstName, String lastName, String email, Car car) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+      this.email = email;
+      this.car = car;
    }
 
+   public Car getCar() {
+      return car;
+   }
+
+   public void setCar(Car car) {
+      this.car = car;
+   }
    public Long getId() {
       return id;
    }
